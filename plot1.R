3ad9c0ca9fb86e7e0338e092ce8cca59a019c124
Qipeng Zhou <xust07@gmail.com>
2016-10-02 21:53:34 +0800

    Add solution code.

---
+++ plot1.R
@@ -0,0 +1,6 @@
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", stringsAsFactors=FALSE)
+data$timeStamp <- strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
+data <- subset(data, (timeStamp >= "2007-02-01") & (timeStamp < "2007-02-03"))
+hist(data$Global_active_power, xlab="Global Active Power (kilowatts)", main="Global Active Power", col="red")
+dev.copy(png, "plot1.png", width=480, height=480, units="px")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,7 @@
+Sys.setlocale(category="LC_TIME", locale="en_US.utf8")
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", stringsAsFactors=FALSE)
+data$timeStamp <- strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
+data <- subset(data, (timeStamp >= "2007-02-01") & (timeStamp < "2007-02-03"))
+with(data, plot(timeStamp, Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab=""))
+dev.copy(png, "plot2.png", width=480, height=480, units="px")
+dev.off()
